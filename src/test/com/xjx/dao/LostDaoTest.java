f7447b6c7d662cba3f11310c6c48c561ab404ad7
熊佳新 <jjxx93@126.com>
2016-09-30 10:33:13 +0800

    9.28 添加了上传图片到七牛云、更改用户头像、验证useruuid等操作

---
+++ src/test/com/xjx/dao/LostDaoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by jiax on 2016/8/22.
+ */
+public class LostDaoTest {
+
+}
---
+++ src/test/com/xjx/service/TestServiceTest.java
@@ -0,0 +1,16 @@
+package com.xjx.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jiax on 2016/8/22.
+ */
+public class TestServiceTest {
+    @Test
+    public void test1() throws Exception {
+
+    }
+
+}
