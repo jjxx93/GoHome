58dcc9f6ec0e6f11f5f0e21a8299e39a8fef1101
熊佳新 <jjxx93@126.com>
2016-11-10 20:06:33 +0800

    - 11.9 检测用户上传照片是否有人脸

---
+++ src/test/cn/gohome/dao/ReportDaoTest.java
@@ -0,0 +1,42 @@
+package cn.gohome.dao;
+
+import cn.gohome.common.GetUUIDNumber;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by jiax on 2016/11/10.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class ReportDaoTest {
+    @Resource
+    private ReportDao reportDao;
+
+    @Test
+    public void insertReport() throws Exception {
+        System.out.println(reportDao.insertReport(GetUUIDNumber.createUUIDNumber(), '0',
+                "b033991d088d426faf38cc40c2996ff7", 1, 0, 0, 0));
+    }
+
+    @Test
+    public void updateReport() throws Exception {
+        reportDao.updateReport('0', "b033991d088d426faf38cc40c2996ff7", 1, 0, 0, 0);
+    }
+
+    @Test
+    public void queryReportByAimTypeAndAimUuid() throws Exception {
+        System.out.println(reportDao.queryReportByAimTypeAndAimUuid('0', "b033991d088d426faf38cc40c2996ff7"));
+    }
+
+    @Test
+    public void queryReportByUuid() throws Exception {
+        System.out.println(reportDao.queryReportByUuid("336b43aa32b9408792447cf583955e71"));
+    }
+
+}
